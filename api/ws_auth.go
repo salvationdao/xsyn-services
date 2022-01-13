20b5b0cef64e0d10326988ede0f95d824524a64e
felix t <41817193+o2bomb@users.noreply.github.com>
2022-01-13 11:12:28 +0800

    Connect facebook ws handler

--- api/ws_auth.go
+++ api/ws_auth.go
@@ -3,14 +3,18 @@ package api
 import (
 	"context"
 	"encoding/json"
+	"net/http"
+	"net/url"
 	"passport"
 	"passport/db"
 	"passport/log_helpers"
 
+	"github.com/gofrs/uuid"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/ninja-software/hub/v2"
 	"github.com/ninja-software/terror/v2"
 	"github.com/rs/zerolog"
+	"github.com/volatiletech/null/v8"
 )
 
 // AuthController holds handlers for roles
@@ -29,6 +33,7 @@ func NewAuthController(log *zerolog.Logger, conn *pgxpool.Pool, api *API) *AuthC
 	}
 
 	api.Command(HubKeyTwitchAuth, authHub.TwitchAuthHandler)
+	api.Command(HubKeyAuthConnectFacebook, authHub.FacebookConnectHandler)
 
 	return authHub
 }
@@ -64,6 +69,76 @@ func (ac *AuthController) TwitchAuthHandler(ctx context.Context, hubc *hub.Clien
 	user, err := db.UserGet(ctx, ac.Conn, passport.UserID(resp.User.Fields().ID()))
 	if err != nil {
 		return terror.Error(err, "failed to query user")
+	}
+
+	reply(user)
+
+	// send user changes to connected clients
+	ac.API.SendToAllServerClient(&ServerClientMessage{
+		Key: UserUpdated,
+		Payload: struct {
+			User *passport.User `json:"user"`
+		}{
+			User: user,
+		},
+	})
+
+	return nil
+}
+
+type NewConnectionRequest struct {
+	*hub.HubCommandRequest
+	Payload struct {
+		Token string `json:"token"`
+	} `json:"payload"`
+}
+
+const HubKeyAuthConnectFacebook hub.HubCommandKey = "AUTH:FACEBOOK:CONNECT"
+
+func (ac *AuthController) FacebookConnectHandler(ctx context.Context, hubc *hub.Client, payload []byte, reply hub.ReplyFunc) error {
+	req := &NewConnectionRequest{}
+	err := json.Unmarshal(payload, req)
+	if err != nil {
+		return terror.Error(err, "Invalid request received")
+	}
+
+	if req.Payload.Token == "" {
+		return terror.Error(terror.ErrInvalidInput, "Facebook token is empty")
+	}
+
+	// Validate Facebook token
+	errMsg := "There was a problem finding a user associated with the provided Facebook account, please check your details and try again."
+	r, err := http.Get("https://graph.facebook.com/me?&access_token=" + url.QueryEscape(req.Payload.Token))
+	if err != nil {
+		return terror.Error(err)
+	}
+	defer r.Body.Close()
+	resp := &struct {
+		ID string `json:"id"`
+	}{}
+	err = json.NewDecoder(r.Body).Decode(resp)
+	if err != nil {
+		return terror.Error(err, errMsg)
+	}
+
+	userID, err := uuid.FromString(hubc.Identifier())
+	if err != nil {
+		return terror.Error(err, "Could not convert user ID to UUID")
+	}
+
+	// Get user
+	user, err := db.UserGet(ctx, ac.Conn, passport.UserID(userID))
+	if err != nil {
+		return terror.Error(err, "failed to query user")
+	}
+
+	// Update user's Facebook ID
+	user.FacebookID = null.StringFrom(resp.ID)
+
+	// Update user
+	err = db.UserUpdate(ctx, ac.Conn, user)
+	if err != nil {
+		return terror.Error(err, errMsg)
 	}
 
 	reply(user)
