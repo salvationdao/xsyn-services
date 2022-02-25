4a1e7cb9d7fc7bb74790ece7520b5b3b927a6472
felix t <41817193+o2bomb@users.noreply.github.com>
2022-02-25 22:45:19 +0800

    Collections filter

--- db/assets.go
+++ db/assets.go
@@ -296,7 +296,7 @@ func AssetGetFromMintContractAndID(ctx context.Context, conn Conn, mintContractA
 	asset := &passport.XsynMetadata{}
 	count := 0
 
-	q := fmt.Sprintf(`SELECT count(*) %s WHERE c."mintContract" = $1 and xsyn_metadata.external_token_id = $2`, AssetGetQueryFrom)
+	q := fmt.Sprintf(`SELECT count(*) %s WHERE LOWER(c."mintContract") = LOWER($1) and xsyn_metadata.external_token_id = $2`, AssetGetQueryFrom)
 
 	err := pgxscan.Get(ctx, conn, &count, q, mintContractAddress, externalTokenID)
 	if err != nil {
@@ -307,7 +307,7 @@ func AssetGetFromMintContractAndID(ctx context.Context, conn Conn, mintContractA
 		return nil, nil
 	}
 
-	q = fmt.Sprintf(`%s WHERE c."mintContract" = $1 and xsyn_metadata.external_token_id = $2`, AssetGetQuery)
+	q = fmt.Sprintf(`%s WHERE  LOWER(c."mintContract") =  LOWER($1) and xsyn_metadata.external_token_id = $2`, AssetGetQuery)
 	err = pgxscan.Get(ctx, conn, asset, q, mintContractAddress, externalTokenID)
 	if err != nil {
 		return nil, terror.Error(err, "Issue getting asset from contract address and token id.")
