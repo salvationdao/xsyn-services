826370fbb3d4bc6b4f77fe633989e46e02af8361
John Nguyen <jtnguyen236@gmail.com>
2022-03-02 09:04:51 +0800

    fix attribute switch removed redundant default case

--- metadata.go
+++ metadata.go
@@ -288,15 +288,15 @@ func ParseWarMachineMetadata(metadata *XsynMetadata, warMachineMetadata *WarMach
 				s, err := strconv.Atoi(att.Value.(string))
 				if err != nil {
 					warMachineMetadata.Speed = 1750
-					passlog.PassLog.Warn().Interface("value",att.Value).Str("metadata_hash", metadata.Hash).Err(err).Msgf("Speed attribute is not a number. Set as default 1750.") /*  */
+					passlog.PassLog.Warn().Interface("value", att.Value).Str("metadata_hash", metadata.Hash).Err(err).Msgf("Speed attribute is not a number. Set as default 1750.") /*  */
 				}
 				warMachineMetadata.Speed = s
 			default:
 				warMachineMetadata.Speed = 1750
-				passlog.PassLog.Warn().Interface("value",att.Value).Str("metadata_hash", metadata.Hash).Msgf("Speed attribute is not a number. Set as default 1750.") /*  */
+				passlog.PassLog.Warn().Interface("value", att.Value).Str("metadata_hash", metadata.Hash).Msgf("Speed attribute is not a number. Set as default 1750.") /*  */
 			}
-		default:
-			warMachineMetadata.Speed = int(att.Value.(float64))
+		// default:
+		// 	warMachineMetadata.Speed = int(att.Value.(float64))
 		case string(WarMachineAttFieldPowerGrid):
 			warMachineMetadata.PowerGrid = int(att.Value.(float64))
 		case string(WarMachineAttFieldShieldRechargeRate):
