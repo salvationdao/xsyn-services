1a1d3041b59ad3242844639430ef25bb9c649520
John Nguyen <jtnguyen236@gmail.com>
2022-03-08 15:33:52 +0800

    implement user updater

--- comms/server_users.go
+++ comms/server_users.go
@@ -1,15 +1,36 @@
 package comms
 
 import (
+	"context"
+	"passport"
+	"passport/db"
+	"passport/passdb"
+
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ninja-software/terror/v2"
+	"github.com/gofrs/uuid"
+	"github.com/volatiletech/null/v8"
 )
 
 type UserReq struct {
-	Addr common.Address
+	ID uuid.UUID
 }
-type UserResp struct{}
+type UserResp struct {
+	ID            uuid.UUID
+	Username      string
+	FactionID     null.String
+	PublicAddress common.Address
+}
 
 func (c *S) User(req UserReq, resp *UserResp) error {
-	return terror.ErrNotImplemented
+	ctx := context.Background()
+	u, err := db.UserGet(ctx, passdb.Conn, passport.UserID(req.ID))
+	if err != nil {
+		return err
+	}
+	resp.FactionID = null.NewString(u.FactionID.String(), u.FactionID == nil)
+	resp.ID = uuid.UUID(u.ID)
+	resp.PublicAddress = common.HexToAddress(u.PublicAddress.String)
+	resp.Username = u.Username
+
+	return nil
 }
