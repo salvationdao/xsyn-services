308275a5cf788d472f20ad325ac7f7ccf365255d
John Nguyen <jtnguyen236@gmail.com>
2022-03-08 10:14:40 +0800

    add deposit and withdraw stubs

---
+++ payments/deposit.go
@@ -0,0 +1,5 @@
+package payments
+
+func GetDeposits() error     { return nil }
+func InsertDeposits() error  { return nil }
+func ProcessDeposits() error { return nil }
---
+++ payments/withdraw.go
@@ -0,0 +1,7 @@
+package payments
+
+import "github.com/ninja-software/terror/v2"
+
+func GetWithdraws() error    { return terror.ErrNotImplemented }
+func InsertWithdraws() error { return terror.ErrNotImplemented }
+func ProcessReleases() error { return terror.ErrNotImplemented }
