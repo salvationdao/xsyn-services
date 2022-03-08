9bd8efd566c5cc5ad719583986c4213320e13225
felix t <41817193+o2bomb@users.noreply.github.com>
2022-03-08 10:42:02 +0800

    Fix make tools for windows, added make db-boiler-windows for windows machines

--- tools/tools_win.go
+++ tools/tools_win.go
@@ -5,9 +5,9 @@ package server
 
 //go:generate go build -o ../bin/migrate.exe -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate
 //go:generate go build -o ../bin/arelo.exe github.com/makiuchi-d/arelo
-//go:generate go build -o ../bin/xcaddy github.com/caddyserver/xcaddy/cmd/xcaddy
-//go:generate go build -o ../bin/sqlboiler github.com/volatiletech/sqlboiler/v4
-//go:generate go build -o ../bin/sqlboiler-psql github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql
+//go:generate go build -o ../bin/xcaddy.exe github.com/caddyserver/xcaddy/cmd/xcaddy
+//go:generate go build -o ../bin/sqlboiler.exe github.com/volatiletech/sqlboiler/v4
+//go:generate go build -o ../bin/sqlboiler-psql.exe github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql
 
 import (
 	_ "github.com/caddyserver/caddy/v2"
