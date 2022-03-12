59a06035762fe536cca314377b4f4ab62338a801
lolivise <tom790924@gmail.com>
2022-03-12 16:50:20 +0800

    add user sups balance check rpc call

--- comms/server_user_stat.go
+++ comms/server_user_stat.go
@@ -6,10 +6,13 @@ import (
 	"passport"
 	"passport/api"
 	"passport/db"
+	"passport/passlog"
 
+	"github.com/gofrs/uuid"
 	"github.com/ninja-software/terror/v2"
 	"github.com/ninja-syndicate/hub"
 	"github.com/ninja-syndicate/hub/ext/messagebus"
+	"github.com/shopspring/decimal"
 )
 
 type UsersGetReq struct {
@@ -61,6 +64,33 @@ func (c *S) SupremacyUserStatSendHandler(req UserStatSendReq, resp *UserStatSend
 
 		go c.MessageBus.Send(ctx, messagebus.BusKey(fmt.Sprintf("%s:%s", api.HubKeyUserStatSubscribe, userStatSend.Stat.ID)), userStatSend.Stat, filterOption)
 	}
+
+	return nil
+}
+
+type UserBalanceGetReq struct {
+	UserID uuid.UUID `json:"userID"`
+}
+
+type UserBalanceGetResp struct {
+	Balance decimal.Decimal `json:"balance"`
+}
+
+func (c *S) SupremacyUserBalanceGetHandler(req UserBalanceGetReq, resp *UserBalanceGetResp) error {
+	balance, err := c.UserCacheMap.Get(req.UserID.String())
+	if err != nil {
+		passlog.L.Error().Str("user_id", req.UserID.String()).Err(err).Msg("Failed to get user balance")
+		return terror.Error(err)
+	}
+
+	// convert balance from big int to decimal
+	b, err := decimal.NewFromString(balance.String())
+	if err != nil {
+		passlog.L.Error().Str("big int amount", balance.String()).Err(err).Msg("Failed to get convert big int to decimal")
+		return terror.Error(err, "failed to convert big int to decimal")
+	}
+
+	resp.Balance = b
 
 	return nil
 }
