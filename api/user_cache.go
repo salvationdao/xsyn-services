b7aef2e22e8d4f859deea97a2f0e67518af81918
Alex Dunmow <alex@ninjasoftware.com.au>
2022-03-14 12:49:19 +0800

    fix args

--- api/user_cache.go
+++ api/user_cache.go
@@ -138,7 +138,7 @@ type UserCacheFunc func(userCacheList UserCacheMap)
 // CreateTransactionEntry adds an entry to the transaction entry table
 func CreateTransactionEntry(conn *sql.DB, nt *passport.NewTransaction) error {
 	q := `INSERT INTO transactions(id ,description, transaction_reference, amount, credit, debit, "group", sub_group)
-				VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);`
+				VALUES($1, $2, $3, $4, $5, $6, $7, $8);`
 
 	_, err := conn.Exec(q, nt.ID, nt.Description, nt.TransactionReference, nt.Amount.String(), nt.To, nt.From, nt.Group, nt.SubGroup)
 	if err != nil {
