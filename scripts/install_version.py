621a33b70c0826e014c3315eb0713e8cdd2eb44c
Nathan Minissale <nathanminissale@gmail.com>
2022-03-28 11:45:40 +0800

    Replace downloaded version script

---
+++ scripts/install_version.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python3
+# Standard Lib
+from email.mime import base
+from fileinput import filename
+import sys
+import os
+import getopt
+import json
+import logging
+import re
+
+# Pip Installs
+from tqdm import tqdm
+import requests
+
+REPO = 'ninja-syndicate/passport-server'
+BASE_URL = "https://api.github.com/repos/{repo}".format(repo=REPO)
+TOKEN = os.environ.get("GITHUB_PAT", "")
+
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"),
+                    format="%(levelname)s: %(message)s")
+log = logging.getLogger("")
+
+
+help_msg = '''Usage: ./install_version.py [options...] <version or "latest">
+  -h, --help        This help message
+  -v, --verbose     Print more logs
+
+Examples:
+
+Get latest
+./install_version.py latest
+
+Get version
+./install_version.py v1.8.5
+
+
+Get latest with verbose logging
+./install_version.py -v latest
+'''
+
+
+def main(argv):
+    log.debug("Parsing input")
+    inputVersion = ''
+    try:
+        opts, args = getopt.getopt(argv, "h::v", ["--help", "--verbose"])
+    except getopt.GetoptError:
+        print(help_msg)
+        sys.exit(2)
+    if len(args) != 1:
+        log.error("There should be one positional argument\n")
+        print(help_msg)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(help_msg)
+            sys.exit()
+        elif opt in ("-v", "--verbose"):
+            log.setLevel(level=logging.DEBUG)
+
+    for arg in args:
+        inputVersion = arg
+
+    log.debug("Finished parsing input")
+
+    asset_meta = download_meta(inputVersion)
+    log.debug(asset_meta)
+    file_name = download_asset(asset_meta)
+    log.info("Downloaded: %s", file_name)
+
+
+def download_meta(version: str):
+    headers = {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': 'token {}'.format(TOKEN),
+        'User-Agent': 'python3 http.client'
+    }
+
+    release_id = version
+    if version != "latest":
+        log.info("Getting releases metadata")
+
+        url = "{base}/releases".format(base=BASE_URL)
+        res = requests.get(url, headers=headers)
+        res.raise_for_status()
+        data = res.content
+        json_data = json.loads(data.decode("utf-8"))
+
+        release_id = json_data[0]["id"]
+
+    log.info("Getting asset metadata")
+
+    url = "{base}/releases/{release_id}".format(
+        base=BASE_URL, release_id=release_id)
+    res = requests.get(url, headers=headers)
+    res.raise_for_status()
+
+    data = res.content
+    json_data = json.loads(data.decode("utf-8"))
+
+    log.debug("asset.id: {}".format(json_data["assets"][0]["id"]))
+    log.debug("asset.name: {}".format(json_data["assets"][0]["name"]))
+    log.debug("asset.url: {}".format(json_data["assets"][0]["url"]))
+
+    return {
+        "id": json_data["assets"][0]["id"],
+        "name": json_data["assets"][0]["name"],
+        "url": json_data["assets"][0]["url"],
+    }
+
+
+def download_asset(asset_meta: dict):
+    log.info("Getting asset: %s", asset_meta["name"])
+    url = "{base}/releases/assets/{release_id}".format(
+        base=BASE_URL, release_id=asset_meta["id"])
+    headers = {
+        'Authorization': 'token {}'.format(TOKEN),
+        'Accept': 'application/octet-stream',
+        'User-Agent': 'python3 http.client'
+    }
+
+    file_name = './{}'.format(asset_meta["name"])
+
+    with requests.get(url, headers=headers, stream=True) as resp:
+        resp.raise_for_status()
+        file_size = int(resp.headers.get("Content-Length"))
+        d = resp.headers['content-disposition']
+        fname = re.findall("filename=(.+)", d)[0]
+        log.info("Downloading: %s", fname)
+        log.debug("code: %s", resp.status_code)
+        log.debug("headers: %s", resp.headers)
+        progress_bar = tqdm(total=file_size, unit='iB', unit_scale=True)
+        with open(file_name, 'wb') as f:
+            for chunk in resp.iter_content(chunk_size=8192):
+                f.write(chunk)
+                progress_bar.update(len(chunk))
+        progress_bar.close()
+
+    return os.path.abspath(file_name)
+
+
+def yes_or_no(question):
+    while "the answer is invalid":
+        reply = str(input(question+' (y/n): ')).lower().strip()
+        if reply[0] == 'y':
+            return True
+        if reply[0] == 'n':
+            return False
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
