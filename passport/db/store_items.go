d3231c14e6b6b216c49bb3ddea638fd0bc3ef802
John Nguyen <jtnguyen236@gmail.com>
2022-04-06 10:06:05 +0800

    prevent refreshing hardcoded fields during a sync

--- passport/db/store_items.go
+++ passport/db/store_items.go
@@ -341,39 +341,17 @@ func refreshStoreItem(storeItemID uuid.UUID, force bool) (*boiler.StoreItem, err
 		return nil, err
 	}
 
-	restrictionGroup, ok := RestrictionMap[resp.TemplateContainer.Template.Tier]
-	if !ok {
-		return nil, fmt.Errorf("restriction not found for %s", resp.TemplateContainer.Template.Tier)
-	}
-
-	if resp.TemplateContainer.BlueprintChassis.Skin == "Slava Ukraini" {
-		restrictionGroup = RestrictionGroupPrize
-	}
-
-	// Golds are prizes only, not purchasable
-	if resp.TemplateContainer.BlueprintChassis.Skin == "Gold" {
-		restrictionGroup = RestrictionGroupPrize
-	}
-	amountAvailable, ok := AmountMap[resp.TemplateContainer.Template.Tier]
-	if !ok {
-		return nil, fmt.Errorf("amountAvailable not found for %s", resp.TemplateContainer.Template.Tier)
-	}
-	priceCents, ok := PriceCentsMap[resp.TemplateContainer.Template.Tier]
-	if !ok {
-		return nil, fmt.Errorf("amountAvailable not found for %s", resp.TemplateContainer.Template.Tier)
-	}
-	count, err := StoreItemPurchasedCount(uuid.Must(uuid.FromString(resp.TemplateContainer.Template.ID)))
-	if err != nil {
-		return nil, fmt.Errorf("get purchase count: %w", err)
-	}
 	dbitem.Data = b
 	dbitem.FactionID = resp.TemplateContainer.Template.FactionID
 	dbitem.RefreshesAt = time.Now().Add(RefreshDuration)
 	dbitem.UpdatedAt = time.Now()
-	dbitem.RestrictionGroup = restrictionGroup
-	dbitem.AmountAvailable = amountAvailable
-	dbitem.UsdCentCost = priceCents
-	dbitem.AmountSold = count
+
+	// Not done on a refresh as it should never be updated via sync after initial insertion
+	// dbitem.RestrictionGroup = restrictionGroup
+	// dbitem.AmountAvailable = amountAvailable
+	// dbitem.UsdCentCost = priceCents
+	// dbitem.AmountSold = count
+
 	dbitem.Tier = resp.TemplateContainer.Template.Tier
 	dbitem.IsDefault = resp.TemplateContainer.Template.IsDefault
 
