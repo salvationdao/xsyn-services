55a0af319d447591d263cf02c085e1048614d4bd
Vinnie <vincent@weareninja.com>
2022-04-19 12:00:42 +0800

    added rpc to get multiplie hashes in one go

--- passport/comms/server_assets.go
+++ passport/comms/server_assets.go
@@ -3,8 +3,11 @@ package comms
 import (
 
 "github.com/ninja-software/terror/v2"
+"strings"
 "xsyn-services/boiler"
-"xsyn-services/passport/passdb""xsyn-services/passport/passlog"
+"xsyn-services/passport/passdb"
+"xsyn-services/passport/passlog"
+)"xsyn-services/passport/passlog"
 
 
 )types2 "xsyn-services/types"
@@ -19,7 +22,6 @@ type AssetOnChainStatusReq struct {
 	AssetHash string `json:"asset_hash"`
 }
 
-//MINTABLE', 'STAKABLE', 'UNSTAKABLE
 
 type AssetOnChainStatusResp struct {
 	OnChainStatus string `json:"on_chain_status"`
@@ -33,5 +35,31 @@ func (s *S) AssetOnChainStatus(req AssetOnChainStatusReq, resp *AssetOnChainStat
 	}
 
 	resp.OnChainStatus = item.OnChainStatus
+	return nil
+}
+
+
+type AssetsOnChainStatusReq struct {
+	AssetHashes []string `json:"asset_hashes"`
+}
+
+
+type AssetsOnChainStatusResp struct {
+	OnChainStatuses map[string]string `json:"on_chain_statuses"`
+}
+
+func (s *S) AssetsOnChainStatus(req AssetsOnChainStatusReq, resp *AssetsOnChainStatusResp) error {
+	items, err := boiler.PurchasedItems(boiler.PurchasedItemWhere.Hash.IN(req.AssetHashes)).All(passdb.StdConn)
+	if err != nil {
+		passlog.L.Error().Str("req.AssetHashes", strings.Join(req.AssetHashes, ", ")).Err(err).Msg("failed to get assets")
+		return terror.Error(err)
+	}
+
+	assetMap := make(map[string]string)
+	for _, asset := range items {
+		assetMap[asset.Hash] = asset.OnChainStatus
+	}
+
+	resp.OnChainStatuses = assetMap
 	return nil
 }
