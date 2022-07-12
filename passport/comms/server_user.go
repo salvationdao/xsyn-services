ad71af0a8b970833fcdaa130bbc728f9daa99758
owengiri20 <owengiri20@gmail.com>
2022-07-12 16:40:31 +0800

    update username

--- passport/comms/server_user.go
+++ passport/comms/server_user.go
@@ -2,10 +2,16 @@ package comms
 
 import (
 	"fmt"
-	"github.com/volatiletech/sqlboiler/v4/boil"
+	"html"
+	"strings"
+	"time"
 	"xsyn-services/boiler"
+	"xsyn-services/passport/helpers"
 	"xsyn-services/passport/passdb"
 	"xsyn-services/passport/passlog"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 
 	"github.com/ninja-software/terror/v2"
 	"github.com/shopspring/decimal"
@@ -97,6 +103,57 @@ func (s *S) UserFactionEnlistHandler(req UserFactionEnlistReq, resp *UserFaction
 	if err != nil {
 		return terror.Error(err, "Failed to update user faction")
 	}
+
+	return nil
+}
+
+type UsernameUpdateReq struct {
+	UserID      string `json:"user_id"`
+	NewUsername string `json:"new_username"`
+	ApiKey      string
+}
+
+func (s *S) UserUpdateUsername(req UsernameUpdateReq, resp *UserResp) error {
+	_, err := IsServerClient(req.ApiKey)
+	if err != nil {
+		return err
+	}
+
+	// get user
+	user, err := boiler.FindUser(passdb.StdConn, req.UserID)
+	if err != nil {
+		passlog.L.Error().Msg("Failed to get user")
+		return terror.Error(err, "unable to get user")
+	}
+
+	if req.NewUsername != "" {
+		// Validate username
+		err = helpers.IsValidUsername(req.NewUsername)
+		if err != nil {
+			passlog.L.Error().Msg("username invalid")
+			return terror.Error(err, "username invalid")
+		}
+
+		bm := bluemonday.StrictPolicy()
+		sanitizedUsername := html.UnescapeString(bm.Sanitize(strings.TrimSpace(req.NewUsername)))
+
+		user.Username = sanitizedUsername
+		// update
+		user.UpdatedAt = time.Now()
+		_, err = user.Update(passdb.StdConn, boil.Infer())
+		if err != nil {
+			passlog.L.Error().Msg("unable to update username")
+			return terror.Error(err, "unable to update username, try again or contact support")
+		}
+
+		resp.ID = user.ID
+		resp.Username = sanitizedUsername
+		resp.FactionID = user.FactionID
+		resp.PublicAddress = user.PublicAddress
+
+	}
+
+	// add to user activity
 
 	return nil
 }
