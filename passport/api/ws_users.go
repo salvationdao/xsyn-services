b0aa844c6c71ae93c478b64a653bd1558de35813
hjpunzalan <hj.punzalan@hotmail.com>
2022-08-17 10:28:59 +0800

    fix email casing for auth and user update

--- passport/api/ws_users.go
+++ passport/api/ws_users.go
@@ -353,19 +353,20 @@ func (uc *UserController) UpdateHandler(ctx context.Context, user *types.User, k
 	oldUser := *user
 
 	if req.Payload.Email.Valid && req.Payload.Email.String != "" {
-		_, err := mail.ParseAddress(req.Payload.Email.String)
+		lowerEmail := strings.ToLower(req.Payload.Email.String)
+		_, err := mail.ParseAddress(lowerEmail)
 		if err != nil {
 			return terror.Error(err, "Invalid email address.")
 		}
 
 		// Check if email address is already taken
-		if req.Payload.Email != user.Email {
-			u, _ := users.Email(req.Payload.Email.String)
+		if null.StringFrom(lowerEmail) != user.Email {
+			u, _ := users.Email(lowerEmail)
 			if u != nil {
 				err = fmt.Errorf("email address is already taken by another user.")
 				return terror.Error(err, "Email address is already taken by another user.")
 			}
-			user.Email = req.Payload.Email
+			user.Email = null.StringFrom(lowerEmail)
 		}
 		user.Verified = true
 	}
