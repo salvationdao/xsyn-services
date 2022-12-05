4b731ea86dfda366bfe5902b0a36e0e7ff442e80
John Nguyen <jtnguyen236@gmail.com>
2022-12-05 17:33:31 +0800

    add migrations for transactions_old indexes

--- migrations/20221205092316_add_indexes_transactions_old.up.sql
+++ migrations/20221205092316_add_indexes_transactions_old.up.sql
@@ -1,3 +1,7 @@
+-- WARNING!!! SLOW AS SHIT MIGRATION
+-- This adds missing indexes to the transactions_old table
+-- This should have already been run in the production and staging database
+
 CREATE INDEX IF NOT EXISTS idx_transactions_old_created_at ON transactions_old(created_at timestamptz_ops);
 CREATE INDEX IF NOT EXISTS idx_transactions_old_created_desc_at ON transactions_old(created_at timestamptz_ops DESC);
 CREATE INDEX IF NOT EXISTS idx_transactions_old_credit ON transactions_old(credit uuid_ops);
